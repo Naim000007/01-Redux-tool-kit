5a52f7997c0a6a562771b9e9fc347c3706a73378
Naim <mdnaim01303202218@gmail.com>
2023-12-04 16:18:27 +0600

    first commit

---
+++ Redux-too-kit/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { DeleteAllUser } from './Components/DeleteAllUser';
+import Navbar from './Components/Navbar';
+import UserDetails from './Components/UserDetails';
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <UserDetails/>
+    <DeleteAllUser/>
+    </>
+  )
+}
+
+export default App
